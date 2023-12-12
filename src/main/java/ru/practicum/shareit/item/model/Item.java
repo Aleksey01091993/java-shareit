216cb04a7b5e4f8f23ecf22e22a5a5125f09307b
Aleksey01091993 <AShvedov1993@yandex.ru>
2023-12-12 18:49:14 +0300

    new

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean available;
+    private String owner;
+    private ItemRequest request;
+
+
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class User {
+    private Long id;
+    private String name;
+    @Email
+    private String email;
+
 }
